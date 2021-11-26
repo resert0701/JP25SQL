7cdd32d914e2961b1af0de91ce4f9bcb1fca6d47
resert0701 <93283879+resert0701@users.noreply.github.com>
2021-11-26 15:46:52 +0100

    inserti u tablicu korisnici,knjige,knjižničari ,posudba
    insert u tablicu

--- knjiznica.sql
+++ knjiznica.sql
@@ -63,31 +63,31 @@ alter table posudba_knjiga add foreign key (posudba_id) references posudba(posud
 
 -- --korisnici 
 
--- insert into korisnik (korisnik_id,ime,prezime,oib,broj_mobitela,adresa,email)
--- values (null,'Rudolf','Esert','83916026127','0958811771','M.A.Reljkovica 5','rudolf.esert@gmail.com');
--- insert into korisnik(korisnik_id,ime,prezime,email,adresa) values
--- (null,'Stjepan','Groznica','stjepan.groznica@gmail.com','Osijek 1'),
--- (null,'Franko','Vekić','franko.vekic@gmail.com','Osijek 2'),
--- (null,'Ivana','Cerovski','ivana.cerovski85@gmail.com','Osijek 3'),
--- (null,'Aleksandar','Jergić','acojergic@gmail.com','Osijek 5'),
--- (null,'Mateo','Cvenić','mateo.cvenic@gmail.com','Osijek 6'),
--- (null,'Juraj','Mak','juraj.mak90@gmail.com','Osijek 7'),
--- (null,'Tea','Matasović','teamatasovic@gmail.com','Osijek 8'),
--- (null,'Miroslav','Ambroš','miroslav.ambros@gmail.com','Osijek 8'),
--- (null,'Matej','Lišćak','liscakmatej1@gmail.com','Osijek 10'),
--- (null,'Filip','Stamenković','stameniwow83@gmail.com','Osijek 11'),
--- (null,'Matej','Zovak','matejzovak031@gmail.com','Osijek 12'),
--- (null,'Antonio','Cukrov','anub031@gmail.com','Osijek 13'),
--- (null,'Ivan','Thür','ivan.thuer@gmail.com','Osijek 14'),
--- (null,'Mario','Kordić','mario.kordic4@gmail.com','Osijek 15'),
--- (null,'Igor','Davidović','igordavidovic137@gmail.com','Osijek 16'),
--- (null,'Patrik','Bolf','bolfpatrik0@gmail.com','Osijek 17'),
--- (null,'Neven','Gostić','neven.gostic@gmail.com','Osijek 18'),
--- (null,'Frane','Dumančić','franedumancic@icloud.com','Osijek 19'),
--- (null,'Filip','Janković','filip.vno.jankovic@gmail.com','Osijek 20'),
--- (null,'Nikolina','Bradarić','nina.bradaric@gmail.com','Osijek 21'),
--- (null,'Josip','Balog','jbalooog@gmail.com','Osijek 22'),
--- (null,'Matija','Skrletović','skrlama@gmail.com','Osijek 23');
+insert into korisnik (korisnik_id,ime,prezime,oib,broj_mobitela,adresa,email)
+values (null,'Rudolf','Esert','83916026127','0958811771','M.A.Reljkovica 5','rudolf.esert@gmail.com');
+insert into korisnik(korisnik_id,ime,prezime,email,adresa) values
+(null,'Stjepan','Groznica','stjepan.groznica@gmail.com','Osijek 1'),
+(null,'Franko','Vekić','franko.vekic@gmail.com','Osijek 2'),
+(null,'Ivana','Cerovski','ivana.cerovski85@gmail.com','Osijek 3'),
+(null,'Aleksandar','Jergić','acojergic@gmail.com','Osijek 5'),
+(null,'Mateo','Cvenić','mateo.cvenic@gmail.com','Osijek 6'),
+(null,'Juraj','Mak','juraj.mak90@gmail.com','Osijek 7'),
+(null,'Tea','Matasović','teamatasovic@gmail.com','Osijek 8'),
+(null,'Miroslav','Ambroš','miroslav.ambros@gmail.com','Osijek 8'),
+(null,'Matej','Lišćak','liscakmatej1@gmail.com','Osijek 10'),
+(null,'Filip','Stamenković','stameniwow83@gmail.com','Osijek 11'),
+(null,'Matej','Zovak','matejzovak031@gmail.com','Osijek 12'),
+(null,'Antonio','Cukrov','anub031@gmail.com','Osijek 13'),
+(null,'Ivan','Thür','ivan.thuer@gmail.com','Osijek 14'),
+(null,'Mario','Kordić','mario.kordic4@gmail.com','Osijek 15'),
+(null,'Igor','Davidović','igordavidovic137@gmail.com','Osijek 16'),
+(null,'Patrik','Bolf','bolfpatrik0@gmail.com','Osijek 17'),
+(null,'Neven','Gostić','neven.gostic@gmail.com','Osijek 18'),
+(null,'Frane','Dumančić','franedumancic@icloud.com','Osijek 19'),
+(null,'Filip','Janković','filip.vno.jankovic@gmail.com','Osijek 20'),
+(null,'Nikolina','Bradarić','nina.bradaric@gmail.com','Osijek 21'),
+(null,'Josip','Balog','jbalooog@gmail.com','Osijek 22'),
+(null,'Matija','Skrletović','skrlama@gmail.com','Osijek 23');
 
 -- ----knjige
 
@@ -111,19 +111,34 @@ values (null,'Josip','Horvat');
 insert into knjiznicar (knjiznicar_id,ime,prezime)
 values (null,'Marija','Dir');
 
--- insert into knjiznicar (knjiznicar_id,ime,prezime)
--- values (null,'Marija','Brkić');
+insert into knjiznicar (knjiznicar_id,ime,prezime)
+values (null,'Kristijan','Petričević');
 
 -- ---posudba
 
--- insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
--- values (null,2,3,1,'2021-11-18');
+insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
+values (null,2,3,1,'2021-11-18');
 
--- insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
--- values (null,1,4,22,'2021-11-19');
+insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
+values (null,1,4,22,'2021-11-19');
 
--- insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
--- values (null,2,2,12,'2021-11-19');
+insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
+values (null,2,2,12,'2021-11-19');
 
--- insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
--- values (null,1,1,15,'2021-11-19');
+insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
+values (null,1,1,15,'2021-11-19');
+
+insert into posudba (posudba_id,knjiznicar_id,knjiga_id,korisnik_id,datum_posudbe)
+values (null,3,2,1,'2021-11-25');
+
+--- posudba_knjiga
+insert into posudba_knjiga(posudba_id,knjiga_id)values 
+(1,3),
+(2,4),
+(3,2),
+(4,1),
+(5,2);
+
+---zakasnina
+
+
